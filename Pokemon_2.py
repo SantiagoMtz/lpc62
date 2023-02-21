fbc167906bdb3b1918a5d26e29d5c7c33d832eca
SantiagoMtz <78572132+SantiagoMtz@users.noreply.github.com>
2023-02-21 09:29:14 -0600

    Update Pokemon_2.py

--- Pokemon_2.py
+++ Pokemon_2.py
@@ -19,3 +19,9 @@ def get_pokemons (url='https://pokeapi.co/api/v2/pokemon-form/', offset=0):
 if __name__ ==  '__main__':
     url =  'https://pokeapi.co/api/v2/pokemon-form/'
     get_pokemons()
+    
+# Script en python que consulta el api de pokemon
+# para listar los nombres de pokemon pero se le agrego
+# interacción para que listaras más pokemons segun se vaya requiriendo.
+# Contribuyo: Santiago Martinez
+# Fecha: Martes 21/02/2023
